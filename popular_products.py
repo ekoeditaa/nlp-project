c93832c897be77dcfb85a4dff0163ed4e035b23b
Eko Edita <itpintusingapura@gmail.com>
2018-10-19 19:42:18 +0800

    [Popular Products] Found top 10 products and reviewers

--- popular_products.py
+++ popular_products.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import json
 
+# Define your dataset path here
 DATA_PATH = './data/reviews_Cell_Phones_and_Accessories_5.json'
 
+# Load the dataset as dataframe
 df = pd.read_json(DATA_PATH, orient="columns", lines=True)
-print(df.head(10))
+
+# Get the top 10 products with most reviews
+print(df['asin'].value_counts()[:10])
+
+# Get the top 10 reviewers who give most reviews
+print(df['reviewerID'].value_counts()[:10])
